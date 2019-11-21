2314209cf5448e5a29ef1536d607e61405c25491
tangweicoffee <tangweicoffee@126.com>
2019-11-21 14:45:04 +0800

    修改时间 14:44

--- hello.js
+++ hello.js
@@ -8,8 +8,8 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // 发送响应数据 "Hello World,will"
-    response.end('Hello World\n');
+    response.end('Hello World,The date is 2019-11-21 14:44\n');
 }).listen(80);
 
 // 终端打印如下信息
-console.log('Server running at http://127.0.0.1:8888/');
+console.log('Server running at http://127.0.0.1:80/');
